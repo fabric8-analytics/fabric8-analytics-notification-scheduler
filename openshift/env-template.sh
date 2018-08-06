b9208281c11c0b513f8d1468073d2e094e22c2be
Redhat <redhat@localhost.localdomain>
2018-08-06 17:25:40 +0530

    Adds env-template file Signed-off-by: Geetika Batra gbatra@redhat.com

---
+++ openshift/env-template.sh
@@ -0,0 +1,9 @@
+#Template with default values used to configure dev deployment of fabric8-analytics
+
+#Openshift configuration
+export OC_URI=''
+# Your dev cluster username
+export OC_USERNAME=''
+# Your dev cluster password
+export OC_PASSWD=''
+export OC_PROJECT="${OC_USERNAME}-cron-fabric8-analytics"
