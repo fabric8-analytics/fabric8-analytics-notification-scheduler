33b8176a0ca92fd6a8ea5dc8997827b6d6c20744
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-10-18 13:27:18 +0200

    TODOs: proper logging in auth module

--- f8a_notification/auth.py
+++ f8a_notification/auth.py
@@ -32,6 +32,7 @@ class Authentication:
     def init_auth_sa_token():
         """Generate service token for authentication."""
         auth_server_url = os.getenv('AUTH_SERVICE_HOST', '')
+        # TODO: add proper logging here 
         print("*****************auth server url ")
         print(auth_server_url)
 
@@ -61,6 +62,8 @@ class Authentication:
                     return access_token
 
             except requests.exceptions.RequestException as e:
+                # TODO: add proper logging here 
                 raise e
 
+        # TODO: add proper logging here 
         raise requests.exceptions.RequestException
