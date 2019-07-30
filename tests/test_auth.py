5e0cc744f8990353981510270178ab00b0881d3c
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-07-30 09:30:20 +0200

    Removed dead code, fixed all other issues

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,7 +1,6 @@
 """Tests for the 'auth' module."""
 
-import json
-from unittest import TestCase, mock
+from unittest import mock
 from auth import Authentication
 import os
 
@@ -41,9 +40,9 @@ def mock_response(flag):
         }
     }
 
-    if flag is "false":
+    if flag == "false":
         return MockResponse(resp, 400, "status_raised")
-    elif flag is "true":
+    elif flag == "true":
         return MockResponse(resp, 200, "")
 
 
